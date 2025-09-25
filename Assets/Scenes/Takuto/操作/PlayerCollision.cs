c6aa326535cb4ba2c10eae508b4567af3d3c1bed
2230018 <2230018@MSI>
2025-09-25 17:05:20 +0900

    ギア(仮)と接触した際にプレイヤーが指定の位置に飛ぶようにした

--- Assets/Scenes/Takuto/操作/PlayerCollision.cs
+++ Assets/Scenes/Takuto/操作/PlayerCollision.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionGear : MonoBehaviour
+public class PlayerCollision : MonoBehaviour
 {
-    // 判定対象をインスペクターで指定できるようにする
-    public GameObject target;
-    // ワープ位置
-    public Vector2 teleportPosition = new Vector2(0, 0);
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +14,6 @@ public class CollisionGear : MonoBehaviour
     void Update()
     {
         
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject == target)
-        {
-            Debug.Log("指定キャラと接触！ 相手は: " + other.gameObject.name);
-
-            // 位置を移動（Vector2 から Vector3 へ変換）
-            other.transform.position = new Vector3(teleportPosition.x, teleportPosition.y, other.transform.position.z);
-        }
     }
 
     private void OnDrawGizmos()
